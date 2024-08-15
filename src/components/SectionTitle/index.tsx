11b3be72435c2cc13e2c37a59633f9190d4b553e
Ahmad Nizar <ahmadnzr.dev@gmail.com>
2024-08-15 16:58:53 +0700

    fix: section title

--- src/components/SectionTitle/index.tsx
+++ src/components/SectionTitle/index.tsx
@@ -4,10 +4,19 @@ import { Text } from "../Text";
 interface Props {
   children: string;
 }
+
 export const SectionTitle = forwardRef<HTMLParagraphElement, Props>(
   ({ children }, ref) => {
     return (
-      <Text ref={ref} $size="lg" $color="gray">
+      <Text
+        ref={ref}
+        $size="sm"
+        $color="gray"
+        style={{
+          textTransform: "uppercase",
+          letterSpacing: "0.2rem",
+        }}
+      >
         {children}
       </Text>
     );
